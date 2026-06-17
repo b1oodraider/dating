fe53396c5ae3ca5c6fdf8ddaee2a06ccc4e325a2
Kamal <kamal12325@mail.ru>
2026-06-17 19:02:38 +0300

    project started, let's gooo

---
+++ dating-core/src/main/java/com/dating/core/DatingCoreApplication.java
@@ -0,0 +1,13 @@
+package com.dating.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingCoreApplication.class, args);
+	}
+
+}
