0f09d7cbf6a4c2ca1ada499e220c783ecd417fdc
Kamal <kamal12325@mail.ru>
2026-07-02 21:53:03 +0300

    fixed test & Nesty 4.2 bugs

---
+++ api-gateway/src/main/java/com/dating/api_gateway/common/web/FallbackController.java
@@ -0,0 +1,21 @@
+package com.dating.api_gateway.common.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+
+    @RequestMapping("/core")
+    public Mono<ResponseEntity<Map<String,String>>> coreFallback() {
+        var response = Map.of("error", "Service unavailable", "message", "сервис временно недоступен");
+        return Mono.just(ResponseEntity.status(503).body(response));
+    }
+}
